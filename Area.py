4efea5c992818afb48150c4ca7270d601075ab96
Pushkin Kachroo <38148643+PushkinKachroo@users.noreply.github.com>
2019-05-07 19:41:10 -0700

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,20 @@
+""" From "COMPUTATIONAL PHYSICS", 3rd Ed, Enlarged Python eTextBook  
+    by RH Landau, MJ Paez, and CC Bordeianu
+    Copyright Wiley-VCH Verlag GmbH & Co. KGaA, Berlin;  Copyright R Landau,
+    Oregon State Unv, MJ Paez, Univ Antioquia, C Bordeianu, Univ Bucharest, 2015.
+    Support by National Science Foundation"""                         
+
+# Area.py: Area of a circle, simple program
+from math import pi
+
+N = 1
+r = 1.
+C = 2.* pi* r 
+A = pi * r**2 
+
+print ('Program number =', N, '\n r, C, A = ', r, C, A)
+ 
+
+""" Expected OUTPUT
+Program number =  1
+r, C, A =  1.0 6.283185307179586 3.141592653589793"""
